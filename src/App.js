d6d8839dd33dbd684e59dd0224b2111652027040
Nayan Gupta <kanumgupta32@gmail.com>
2024-01-15 12:56:44 +0530

    Modified button and navbar and added hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom" ;
 import Navbar from "./Navbar"
+import HeroSection from "./HeroSection";
 
 function App() {
   return(
     <>
     <Router>
       <Navbar/>
+      <HeroSection/>
     </Router>
     </>
   );
